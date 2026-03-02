df8fa745241918c1117e3444128cce38de5aa11a
Mathias Phillip <mkpstudent@gmail.com>
2026-03-02 22:53:15 +0100

    First commit

---
+++ QuietRiot/QuietRiotApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuietRiotApp.swift
+//  QuietRiot
+//
+//  Created by Mathias Klintebjerg Phillip on 02/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct QuietRiotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
